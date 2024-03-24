13e93a79cbf94e8a512a16a155c867e32e4fe9a5
Tunzeki <tundewebhead@gmail.com>
2024-03-24 04:07:53 +0100

    Create base_table to import existing data into

---
+++ queries.sql
@@ -0,0 +1,33 @@
+----------------------------------------------------
+-- Use littlelemon database
+----------------------------------------------------
+USE littlelemon;
+
+----------------------------------------------------
+-- Table base_table
+----------------------------------------------------
+
+-- Create a base_table which the existing data
+-- collected by Little Lemon will be imported into
+CREATE TABLE IF NOT EXISTS base_table (
+    RowNumber INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    OrderID VARCHAR(45) NOT NULL,
+    OrderDate DATE NOT NULL,
+    DeliveryDate DATE NOT NULL,
+    CustomerID VARCHAR(45) NOT NULL,
+    CustomerName VARCHAR(45) NOT NULL,
+    City VARCHAR(45) NOT NULL,
+    Country VARCHAR(45) NOT NULL,
+    PostalCode VARCHAR(45) NOT NULL,
+    Cost DECIMAL(6,2) NOT NULL,
+    Sales DECIMAL(6,2) NOT NULL,
+    Quantity INT NOT NULL,
+    Discount DECIMAL(6,2) NOT NULL,
+    DeliveryCost DECIMAL(6,2) NOT NULL,
+    CourseName VARCHAR(45) NOT NULL,
+    CuisineName VARCHAR(45) NOT NULL,
+    StarterName VARCHAR(45) NOT NULL,
+    DessertName VARCHAR(45) NOT NULL,
+    Drink VARCHAR(45) NOT NULL,
+    Sides VARCHAR(45) NOT NULL
+) ENGINE=InnoDB;
