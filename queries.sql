80630f380c0ba04f27f3e2792b848911c0f313e5
Tunzeki <tundewebhead@gmail.com>
2024-03-26 17:03:35 +0100

    Create INSERT INTO SELECT statements

--- queries.sql
+++ queries.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS base_table (
     PostalCode VARCHAR(45) NOT NULL,
     CountryCode VARCHAR(10) NOT NULL,
     Cost DECIMAL(6,2) NOT NULL,
-    Sales DECIMAL(6,2) NOT NULL,
+    Sales DECIMAL(6,3) NOT NULL,
     Quantity INT NOT NULL,
     Discount DECIMAL(6,2) NOT NULL,
     DeliveryCost DECIMAL(6,2) NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS base_table (
     DessertName VARCHAR(45) NOT NULL,
     Drink VARCHAR(45) NOT NULL,
     Sides VARCHAR(45) NOT NULL
-) ENGINE=InnoDB;
+) ENGINE=InnoDB,CHARSET=utf8mb4;
 
 
 -- Find out the location to keep the CSV file to be imported
@@ -52,3 +52,61 @@ IGNORE 1 ROWS
 )
 SET OrderDate = STR_TO_DATE(@OrderDate, '%m/%d/%Y'), 
 DeliveryDate = STR_TO_DATE(@DeliveryDate, '%m/%d/%Y');
+
+
+------------------------------------------------------------------------------
+-- INSERT INTO SELECT
+------------------------------------------------------------------------------
+
+-- INSERT INTO Customers ...
+INSERT INTO Customers (CustomerID, FirstName, LastName)
+SELECT DISTINCT CustomerID, SUBSTRING_INDEX(CustomerName, ' ', 1), SUBSTRING_INDEX(CustomerName, ' ', -1)
+FROM base_table;
+
+
+-- INSERT INTO Countries ...
+INSERT INTO Countries (Country, CountryCode)
+SELECT DISTINCT Country, CountryCode
+FROM base_table;
+
+
+-- INSERT INTO Cities ...
+INSERT INTO Cities (City)
+SELECT DISTINCT City
+FROM base_table;
+
+
+-- INSERT INTO Courses ...
+INSERT INTO Courses (CourseName)
+SELECT DISTINCT CourseName
+FROM base_table;
+
+
+-- INSERT INTO Cuisines ...
+INSERT INTO Cuisines (CuisineName)
+SELECT DISTINCT CuisineName
+FROM base_table;
+
+
+-- INSERT INTO Starters ...
+INSERT INTO Starters (StarterName)
+SELECT DISTINCT StarterName
+FROM base_table;
+
+
+-- INSERT INTO Desserts ...
+INSERT INTO Desserts (DessertName)
+SELECT DISTINCT DessertName
+FROM base_table;
+
+
+-- INSERT INTO Drinks ...
+INSERT INTO Drinks (DrinkName)
+SELECT DISTINCT Drink
+FROM base_table;
+
+
+-- INSERT INTO Sides ...
+INSERT INTO Sides (SideName)
+SELECT DISTINCT Sides
+FROM base_table;
